d24877e91b9b0ac5a5c54e00d24db6c790604520
sujal8454 <sujal8454verma@gmail.com>
2025-10-13 18:03:22 +0530

    Update styleteller.py

--- styleteller.py
+++ styleteller.py
@@ -360,7 +360,7 @@ except Exception:
 
 # Insert top-center logo (will fade in with page)
 try:
-    st.markdown('<div id="styleteller-logo-wrap"><img src="https://i.ibb.co/3YMDZQVn/logo.png" alt="StyleTeller" /></div>', unsafe_allow_html=True)
+    st.markdown('<div class="logo-container"><img src="logo.png" alt="Logo"></div>', unsafe_allow_html=True)
 except Exception:
     pass
 
